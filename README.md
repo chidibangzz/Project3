fb0cd981598064bbdd5c4dc47a5e77ba045942ce
Chidi Ekeke <chidiekeke18@yahoo.com>
2020-10-31 20:40:21 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Project3
+# Project 3
+03Advanced Javascript: Random Password Generator
+
+
+Description of Portfolio
+
+Password prompts will display asking user how long they want their password to be, what type of characters and numbers they want paswword to contain. User has to pick atleast one of the confirm boxes for password generator to work.
+
+Languages used
+Javascript and html
+
+Links
+Git Hub Repo
+https://github.com/chidibangzz/Project3
+Contributing
+Pull request are welcome. For major change, please open an issue first to dicuss what you would like to change.
+
+ https://chidibangzz.github.io/Project3/ Language Javascript Last updated 2 hours ago https://github.com/chidibangzz/chidiekeke.github.io|chidibangzz/chidiekeke.github.iochidibangzz/chidiekeke.github.io | Oct 30 | Added by GitHub
--- script.js
+++ script.js
@@ -6,27 +6,27 @@ var copyBtn = document.querySelector("#copy");
 //function that generates password
 function generatePassword() {
   var passwordChar = [];
- 
-  var finalPassword= "";
-//var welcome = confirm("Would you like a password")
+
+  var finalPassword = "";
+  //var welcome = confirm("Would you like a password")
 
 
-var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "{", "[", "}", "]", ":", ";", "|", "<", ",", ">", "?", "/"];
+  var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "{", "[", "}", "]", ":", ";", "|", "<", ",", ">", "?", "/"];
 
 
   var passlength = parseInt(prompt("How many characters would you like to contain"))
 
-  if (passlength < 8 || passlength> 128) {
+  if (passlength < 8 || passlength > 128) {
     alert('Your password must be between 8 and 128 characters')
     return;
 
-} else{alert("your password will be "+passlength+" characters long")} 
+  } else { alert("your password will be " + passlength + " characters long") }
 
   var isSpecialChar = confirm("Would you like special characters?");
-  
+
   var isUpperCase = confirm("Would you like upper case?");
 
   var isLowerCase = confirm("Would you like lowercase?");
@@ -34,40 +34,40 @@ var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "
   var isNumberChar = confirm("Would you like numbers?");
 
 
-// if user doesnt select anything
+  // if user doesnt select anything
 
-if(!isLowerCase && !isUpperCase && !isNumberChar && !isSpecialChar){
-  alert("You must chose one of the character types to proceed");
-  return;
-}
+  if (!isLowerCase && !isUpperCase && !isNumberChar && !isSpecialChar) {
+    alert("You must chose one of the character types to proceed");
+    return;
+  }
 
 
-//password based on users response
-  if( isLowerCase ) {
-    for (var i= 0; i<lowerCase.length; i++) {
+  //password based on users response possible outcomes
+  if (isLowerCase) {
+    for (var i = 0; i < lowerCase.length; i++) {
       passwordChar.push(lowerCase[i]);
     }
   }
 
-  if( isUpperCase ) {
-    for (var i= 0; i<upperCase.length; i++) {
+  if (isUpperCase) {
+    for (var i = 0; i < upperCase.length; i++) {
       passwordChar.push(upperCase[i]);
     }
   }
-  if( isNumberChar ) {
-    for (var i= 0; i<numberChar.length; i++) {
+  if (isNumberChar) {
+    for (var i = 0; i < numberChar.length; i++) {
       passwordChar.push(numberChar[i]);
     }
   }
-  if( isSpecialChar ) {
-    for (var i= 0; i<specialChar.length; i++) {
+  if (isSpecialChar) {
+    for (var i = 0; i < specialChar.length; i++) {
       passwordChar.push(specialChar[i]);
     }
   }
 
   //rRun a loop for user
-  for (var i=0; i<passlength; i++) {
-    var index = Math.floor(Math.random()*passwordChar.length);
+  for (var i = 0; i < passlength; i++) {
+    var index = Math.floor(Math.random() * passwordChar.length);
 
     finalPassword = finalPassword + passwordChar[index];
 
@@ -82,7 +82,7 @@ if(!isLowerCase && !isUpperCase && !isNumberChar && !isSpecialChar){
 
 function writePassword() {
   var password = generatePassword();
-  
+
 
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -91,7 +91,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
   //if(user wants numbers) {
 
-   
+
 
 
 
